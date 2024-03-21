9955dadc86f50c4c85da60f829e66fa4b3828671
TapanPadaliya <tapanpadaliya02@gmail.com>
2024-03-21 14:24:22 +0530

    vayu fixes

--- app.vue
+++ app.vue
@@ -4,6 +4,13 @@
     <div class="layout-parent">
       <NuxtLayout />
     </div>
+    <VyPort
+      name="modal"
+      has-overlay
+      class="port--center p-40 z-50"
+      enter-active-class="animate__animated animate__faster animate__fadeIn"
+      leave-active-class="animate__animated animate__faster animate__fadeOut"
+    />
   </div>
 </template>
 <script setup></script>
--- pages/test.vue
+++ pages/test.vue
@@ -17,6 +17,21 @@
         enter-active-class="animate__animated animate__faster animate__fadeInUp"
         leave-active-class="animate__animated animate__faster animate__fadeOutUp"
       />
+
+      <VyButton
+        label="Open Modal"
+        class="button--md button--primary button--solid button--rounded text-black"
+        @click="$vayu.modal.open('hello')"
+      />
+
+      <VyModal name="hello" title="title" class="bg-white">
+        <template #default>
+          <div class="p-8">This is Body!</div>
+        </template>
+        <template #footer>
+          <div class="p-3">This is Footer</div>
+        </template>
+      </VyModal>
     </ClientOnly>
   </div>
 </template>
